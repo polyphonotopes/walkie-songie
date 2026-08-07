900585ce22f2a10f0b010176c2eb47785b598308
Micah <micahscopes@gmail.com>
2026-08-07 14:30:19 +0200

    Set up iroh/hhhs build: nix wasm toolchain, pin hardened kernel bd23d4e, add browser-net feature

---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  description = "walkie-songie Tauri/Iroh development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [(import rust-overlay)];
+        };
+        rust = pkgs.rust-bin.stable."1.97.1".default.override {
+          extensions = ["clippy" "rustfmt"];
+          targets = ["wasm32-unknown-unknown"];
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            rust
+            trunk
+            nodejs_24
+            pnpm
+            pkg-config
+            wrapGAppsHook3
+            llvmPackages.llvm
+          ];
+
+          # `ring` (iroh's tls-ring, pulled in by the wasm `browser-net`
+          # feature) compiles C for wasm32-unknown-unknown. The nix-wrapped
+          # clang injects native glibc flags that break wasm objects, so point
+          # the cc crate at the UNWRAPPED clang + llvm-ar explicitly.
+          CC_wasm32_unknown_unknown = "${pkgs.llvmPackages.clang-unwrapped}/bin/clang";
+          AR_wasm32_unknown_unknown = "${pkgs.llvmPackages.llvm}/bin/llvm-ar";
+
+          buildInputs = with pkgs; [
+            alsa-lib
+            glib
+            gtk3
+            libayatana-appindicator
+            librsvg
+            libsoup_3
+            openssl
+            udev
+            webkitgtk_4_1
+          ];
+
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            pkgs.alsa-lib
+            pkgs.libayatana-appindicator
+            pkgs.udev
+          ];
+        };
+      }
+    );
+}
